9f9b5e6928e30f770cea07ebd9e2304b431d64a3
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-16 11:20:41 +0530

    Props in Classes

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -14,7 +14,9 @@ function App() {
             <button>Action</button>
         </Greet>
         <Greet name="Diana" heroName="Wonder Woman"/>
-        { /* <Welcome /> */}
+        <Welcome name="Bruce" heroName="Batman"/>
+        <Welcome name="Clark" heroName="Superman"/>
+        <Welcome name="Diana" heroName="Wonder Woman"/>
         { /* <Hello /> */}
     </div>
   )
