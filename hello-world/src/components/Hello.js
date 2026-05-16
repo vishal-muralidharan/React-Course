4a3964f05241b1fc96ab3a72c9e8680ba8751ff3
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-16 10:50:55 +0530

    Used createElement Function

--- hello-world/src/components/Hello.js
+++ hello-world/src/components/Hello.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Hello = () => {
-    return (
-        <h1>Hello, Vishal</h1>
-    )
+    // return (
+    //    <h1>Hello, Vishal</h1>
+    // )
+
+    return React.createElement('div', null, 'Hello, Vishal')
 }
 
 export default Hello
