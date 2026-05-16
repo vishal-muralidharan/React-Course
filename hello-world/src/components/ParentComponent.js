782e0d79f2be1abbda85576dfeb8304dcd326625
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-16 13:12:09 +0530

    Methods as Props - II

--- hello-world/src/components/ParentComponent.js
+++ hello-world/src/components/ParentComponent.js
@@ -12,8 +12,8 @@
         this.greetParent = this.greetParent.bind(this)
     }
 
-    greetParent() {
-        alert(`Hello ${this.state.parentName}`)
+    greetParent(childName) {
+        alert(`Hello ${this.state.parentName} from ${childName}`)
     }
 
      render() {
