55edc02191452beb28637d19a44817f568b71101
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-16 11:54:35 +0530

    Using and Changing State

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-        <Greet name="Bruce" heroName="Batman">
+      <Message />
+        { /* <Greet name="Bruce" heroName="Batman">
             <p>This is props children</p>
         </Greet>
         <Greet name="Clark" heroName="Superman">
@@ -17,7 +19,7 @@ function App() {
         <Welcome name="Bruce" heroName="Batman"/>
         <Welcome name="Clark" heroName="Superman"/>
         <Welcome name="Diana" heroName="Wonder Woman"/>
-        { /* <Hello /> */}
+        <Hello /> */}
     </div>
   )
 }
