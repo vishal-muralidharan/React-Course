4d5c72046b04183c9138b86b7ffd49d30366ba17
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-16 17:20:30 +0530

    Fragment - II

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import FragmentDemo from './components/FragmentDemo'
+import Table from './components/Table'
 
 function App() {
   return (
     <div className="App">
-        <FragmentDemo />
+        <Table />
     </div>
   )
 }
---
+++ hello-world/src/components/Columns.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Columns() {
+    return (
+        <>
+            <td>Name</td>
+            <td>Vishal</td>
+        </>
+    )
+}
+
+export default Columns
