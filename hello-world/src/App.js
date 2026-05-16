c18dcda2bce97d63393c5973451854cc25756f2f
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-16 17:15:07 +0530

    Fragment - Intro

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import LifecycleA from './components/LifecycleA'
+import FragmentDemo from './components/FragmentDemo'
 
 function App() {
   return (
     <div className="App">
-        <LifecycleA />
+        <FragmentDemo />
     </div>
   )
 }
