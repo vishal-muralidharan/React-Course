7e968a038eba0215cbdba414afc897e7a3032aae
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-16 10:50:31 +0530

    Basic JSX Code

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import Hello from './components/Hello'
 
 function App() {
   return (
     <div className="App">
         <Greet />
         <Welcome />
+        <Hello />
     </div>
-  );
+  )
 }
 
 export default App;
