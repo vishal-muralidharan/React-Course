424555a53843e665a47e3ae0a38535624fe1a399
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-16 15:13:03 +0530

    Index Keys in List Rendering

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -3,16 +3,19 @@ import Person from './Person'
 
 function NameList() {
     const Names = ['Bruce', 'Diana', 'Clark']
+
     const Persons = [
                     { id: 1, name: 'Bruce', age: 28, skill: 'Vue' },
                     { id: 2, name: 'Clark', age: 32, skill: 'React' },
                     { id: 3, name: 'Diana', age: 27, skill: 'Angular' }
                     ]
-    const PersonsList = Persons.map(person => <Person key={person.id} person={person} />)
+
+    const nameList = Names.map((name, index) => <h2 key={index}>{index} {name}</h2>)
+
     return (
         <div>
             {
-                PersonsList
+                nameList
             }
         </div>
     )
