b8b307c0bd914421523757ff22730da913c99847
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-16 13:10:03 +0530

    Methods as Props - I

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -8,11 +8,14 @@ import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
 import EventBind from './components/EventBind'
+import ParentComponent from './components/ParentComponent'
+
 
 function App() {
   return (
     <div className="App">
-        <EventBind />
+        <ParentComponent />
+        { /* <EventBind /> */ }
         { /* <FunctionClick />}
         <ClassClick /> */ }
         { /*<Greet name="Bruce" heroName="Batman"/>
