48231c37dd9a1f32dd1c65ca5bbc37766e4be4d2
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-18 09:17:44 +0530

    Refs - Focus

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import ParentComponent from './components/ParentComponent'
+import RefsDemo from './components/RefsDemo'
 
 function App() {
   return (
     <div className="App">
-        <ParentComponent />
+        <RefsDemo />
     </div>
   )
 }
