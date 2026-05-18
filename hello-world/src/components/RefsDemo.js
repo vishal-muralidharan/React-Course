2b2cc29e3e93cde5907716c24d0fa57d7a7df5a8
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-18 09:27:37 +0530

    Refs - Callback Refs

--- hello-world/src/components/RefsDemo.js
+++ hello-world/src/components/RefsDemo.js
@@ -5,11 +5,20 @@ export class RefsDemo extends Component {
       super(props)
     
       this.inputRef = React.createRef()
+
+      this.cbRef = null
+      this.setCbRef = (element) => {
+        this.cbRef = element
+      }
     }
 
     componentDidMount() {
-        this.inputRef.current.focus()
-        console.log(this.inputRef)
+        /* this.inputRef.current.focus()
+        console.log(this.inputRef) */
+
+        if (this.cbRef) {
+            this.cbRef.focus()
+        }
     }
 
     clickHandler = () => {
@@ -20,6 +29,7 @@ export class RefsDemo extends Component {
         return (
         <div>
             <input type='text' ref={this.inputRef}></input>
+            <input type='text' ref={this.setCbRef}></input>
             <button onClick={this.clickHandler}>Click</button>
         </div>
         )
