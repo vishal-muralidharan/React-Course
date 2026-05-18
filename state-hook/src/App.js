55ba97af4132ba4a129480dcb6a8718616a10d2c
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-18 17:00:57 +0530

    useEffect Hook - VI

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import IntervalHookCounter from './components/IntervalHookCounter';
+import DataFetching from './components/DataFetching';
 
 function App() {
     return (
         <div className="App">
-            <IntervalHookCounter />
+            <DataFetching />
         </div>
     );
 }
