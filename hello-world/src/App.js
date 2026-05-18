4009b16ee0a1d745d8f43d484f226cfa7ac5158d
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-18 09:35:08 +0530

    Refs with Class

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import RefsDemo from './components/RefsDemo'
+import FocusInput from './components/FocusInput'
 
 function App() {
   return (
     <div className="App">
-        <RefsDemo />
+        <FocusInput />
     </div>
   )
 }
