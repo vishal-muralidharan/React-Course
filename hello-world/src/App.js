dd45df7f1d46769afbad14211dfe5af2591d7dbc
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-18 11:39:45 +0530

    Render Props - II

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import ClickCounterTwo from './components/ClickCounterTwo'
 import HoverCounterTwo from './components//HoverCounterTwo'
+import User from './components//User'
 
 function App() {
   return (
     <div className="App">
       <ClickCounterTwo />
       <HoverCounterTwo />
+      <User render={(isLoggedIn) => isLoggedIn ? "Vishal" : "Guest"}/>
     </div>
   )
 }
--- hello-world/src/components/ClickCounterTwo.js
+++ hello-world/src/components/ClickCounterTwo.js
@@ -1,20 +1,6 @@
 import React, { Component } from 'react'
 
 class ClickCounterTwo extends Component {
-    constructor(props) {
-        super(props)
-        
-        this.state = {
-            count: 0
-        }
-    }
-     
-    incrementCount = () => {
-        this.setState(prevstate => {
-            return {count: prevstate.count + 1}
-        })
-    }
-
     render() {
         const {count} = this.state
         return (
