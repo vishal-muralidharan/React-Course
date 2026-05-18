6101c6377c967c9c15b52af7df8178e713f84336
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-18 17:52:52 +0530

    useReducer Hook - III

--- state-hook/src/components/CounterTwo.js
+++ state-hook/src/components/CounterTwo.js
@@ -1,15 +1,20 @@
 import React, {useReducer} from 'react'
 
 const initial = {
-    first: 0
+    first: 0,
+    second: 10
 }
 
 const reducer = (state, action) => {
     switch(action.type) {
         case 'increment' :
-            return {first: state.first + 1}
+            return {...state, first: state.first + action.value}
         case 'decrement' :
-            return {first: state.first - 1}   
+            return {...state, first: state.first - action.value}  
+        case 'increment2' :
+            return {...state, second: state.second + action.value}
+        case 'decrement2' :
+            return {...state, second: state.second - action.value} 
         case 'reset' :
             return initial
         default :
@@ -22,9 +27,16 @@ function CounterOne() {
 
     return (
         <div>
-            <div>{count.first}</div>
-            <button onClick={() => dispatch({type: 'increment'})}>Increment</button>
-            <button onClick={() => dispatch({type: 'decrement'})}>Decrement</button>
+            <div>First Counter: {count.first}</div>
+            <div>Second Counter: {count.second}</div>
+            <button onClick={() => dispatch({type: 'increment', value: 1})}>Increment</button>
+            <button onClick={() => dispatch({type: 'decrement', value: 1})}>Decrement</button>
+            <button onClick={() => dispatch({type: 'increment', value: 5})}>Increment 5</button>
+            <button onClick={() => dispatch({type: 'decrement', value: 5})}>Decrement 5</button>
+
+            <button onClick={() => dispatch({type: 'increment2', value: 1})}>Increment C2</button>
+            <button onClick={() => dispatch({type: 'decrement2', value: 1})}>Decrement C2</button>
+
             <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
         </div>
     )
