ca37bf4e1e1104194e82fffb3f36c4507cd35ceb
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-18 13:26:30 +0530

    useState Hook - III

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
 
 function App() {
     return (
         <div className="App">
-            <HookCounterTwo />
+            <HookCounterThree />
         </div>
     );
 }
