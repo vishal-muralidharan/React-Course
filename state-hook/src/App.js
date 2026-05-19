993b068d7c0f0caf6672357797919e54b4a11869
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-19 09:49:49 +0530

    useReducer Hook - Fetch Data - I

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,40 +1,12 @@
 import React, {useReducer} from 'react';
 import './App.css';
-import A from './components/5. useReducer with useContext/A';
-import B from './components/5. useReducer with useContext/B';
-import C from './components/5. useReducer with useContext/C';
-
-export const CountContext = React.createContext()
-
-const initial = 0
-const reducer = (state, action) => {
-    switch(action) {
-        case 'increment' :
-            return state + 1
-        case 'decrement' :
-            return state - 1   
-        case 'reset' :
-            return initial
-        default :
-            return state
-    }
-}
-
+import DataFetchingOne from './components/DataFetchingOne';
 
 function App() {
-    const [count, dispatch] = useReducer(reducer, initial)
-
     return (
-        <CountContext.Provider 
-            value={{countState: count, countDispatch: dispatch}}
-        >
-            <div className="App">
-                Count: {count}
-                <A />
-                <B />
-                <C />
-            </div>
-        </CountContext.Provider>
+        <div>
+            <DataFetchingOne />
+        </div>
     );
 }
 
