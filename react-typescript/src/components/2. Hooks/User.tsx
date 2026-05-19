7e362547f6ba9f7c16b1b53d6732fbd77ede30ff
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-19 17:37:31 +0530

    TypeScript - useState Hook - III

--- react-typescript/src/components/2. Hooks/User.tsx
+++ react-typescript/src/components/2. Hooks/User.tsx
@@ -6,7 +6,7 @@ type AuthUser = {
 }
 
 export const User = () => {
-    const [user, setUser] = useState<AuthUser | null>(null)
+    const [user, setUser] = useState<AuthUser>({} as AuthUser)
 
     const handleLogin = () => {
         setUser({
@@ -15,16 +15,11 @@ export const User = () => {
         })
     }
 
-    const handleLogout = () => {
-        setUser(null)
-    }
-
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <div>User name is {user?.name}</div>
-            <div>User email is {user?.email}</div>
+            <div>User name is {user.name}</div>
+            <div>User email is {user.email}</div>
         </div>
     )
 }
