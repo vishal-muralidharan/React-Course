9b0ac363915bcb546effef1627be2f16fd94504a
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-19 11:01:32 +0530

    useDocumentTitle Hook - I

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import HookTimer from './components/HookTimer';
+import DocTitleOne from './components/DocTitleOne';
+import DocTitleTwo from './components/DocTitleTwo';
 
 function App() {
     return (
         <div>
-            <HookTimer />
+            <DocTitleOne />
+            <DocTitleTwo />
         </div>
     );
 }
