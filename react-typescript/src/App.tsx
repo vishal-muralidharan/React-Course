415a54406788261694c0d9d6052739ce59578aaa
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-19 17:00:53 +0530

    Typescript - Props - IV

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,20 +1,16 @@
 import './App.css';
-import { Heading } from './components/Heading';
-import { Status } from './components/Status';
-import { Oscar } from './components/Oscar';
-import { Greet } from './components/Greet';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
+
 
 function App() {
     return (
       <div className="App">
-          <Status status='loading'/>
-          <Heading>
-              Placeholder Text
-          </Heading>
-          <Oscar>
-              <Heading>Oscar goes to Lionel Messi!</Heading>
-          </Oscar>
-          <Greet name='Vishal' isLoggedIn={true} />
+          <Button handleClick={(event, id) => {
+              console.log('Button Clicked!', event)
+          }}/>
+
+          <Input value='' handleChange={(event) => console.log(event)}/>
       </div>
     );
 }
---
+++ react-typescript/src/components/Input.tsx
@@ -0,0 +1,12 @@
+type InputProps = {
+    value: string, 
+    handleChange: (event:React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export const Input = (props:InputProps) => {
+    return (
+        <div>
+            <input type='text' value={props.value} onChange={props.handleChange} />
+        </div>
+    )
+}
