55529bfc2e1eace70b0a4439d8c0ade623118344
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-20 09:27:30 +0530

    TypeScript - useReducer Hook - II

--- react-typescript/src/components/2. Hooks/Counter.tsx
+++ react-typescript/src/components/2. Hooks/Counter.tsx
@@ -4,21 +4,29 @@ type CounterState = {
     count: number
 }
 
-type CounterAction = {
-    type: string,
+type UpdateAction = {
+    type: 'increment' | 'decrement',
     payload: number
 }
+
+type ResetAction = {
+    type: 'reset',
+}
+
+type CounterAction = UpdateAction | ResetAction
 
 const initialState = { count: 0 }
 
 function reducer(state: CounterState, action: CounterAction) {
     switch (action.type) {
         case 'increment':
-        return { count: state.count + action.payload }
+            return { count: state.count + action.payload }
         case 'decrement':
-        return { count: state.count - action.payload }
+            return { count: state.count - action.payload }
+        case 'reset':
+            return initialState
         default:
-        return state
+            return state
     }
 }
 
@@ -33,6 +41,9 @@ export const Counter = () => {
             </button>
             <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>
                 Decrement 10
+            </button>
+            <button onClick={() => dispatch({ type: 'reset'})}>
+                Reset
             </button>
         </>
     )
