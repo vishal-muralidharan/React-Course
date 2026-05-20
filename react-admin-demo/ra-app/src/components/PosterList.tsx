32701257a0e41529bab8ef2f288b506cdbd8f3a3
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-20 13:29:01 +0530

    React-Admin - V

--- react-admin-demo/ra-app/src/components/PosterList.tsx
+++ react-admin-demo/ra-app/src/components/PosterList.tsx
@@ -1,13 +1,25 @@
 import {
-    Datagrid,
+    DatagridConfigurable,
     ImageField,
     List,
     NumberField,
     ReferenceField,
     TextField,
     useRecordContext,
+    TopToolbar,
+    SelectColumnsButton,
+    ExportButton,
+    CreateButton,
 } from 'react-admin';
 import { PosterFilterSidebar } from './FilterList';
+
+const PosterListActions = () => (
+    <TopToolbar>
+        <SelectColumnsButton />
+        <CreateButton />
+        <ExportButton />
+    </TopToolbar>
+);
 
 const UrlField = ({ source }: { source: string }) => {
     const record = useRecordContext();
@@ -16,8 +28,8 @@ const UrlField = ({ source }: { source: string }) => {
 };
 
 export const PosterList = () => (
-    <List aside={<PosterFilterSidebar />}>
-        <Datagrid rowClick="edit">
+    <List aside={<PosterFilterSidebar />} actions={<PosterListActions />}>
+        <DatagridConfigurable rowClick="edit">
             <TextField source="id" />
             <ReferenceField source="category_id" reference="categories">
                 <TextField source="name" />
@@ -37,6 +49,6 @@ export const PosterList = () => (
             <TextField source="description" />
             <NumberField source="stock" />
             <NumberField source="sales" sx={{ fontWeight: "bold" }} />
-        </Datagrid>
+        </DatagridConfigurable>
     </List>
 );
