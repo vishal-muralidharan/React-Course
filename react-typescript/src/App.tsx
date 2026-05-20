82778ff82b4e97f90398593e276c91a6c4161d7b
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-20 09:42:40 +0530

    TypeScript - useContext Hook - I

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
 import { Counter } from './components/2. Hooks/Counter';
+import { Box } from './components/3. Context/Box';
+import { ThemeContextProvider } from './components/3. Context/ThemeContext';
 
 function App() {
     return (
         <div className="App">
-            <Counter />
+            <ThemeContextProvider>
+                <Box />
+            </ThemeContextProvider> 
         </div>
     );
 }
